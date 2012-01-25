b8d67a3afddb7a500890f4d468d519242a1ae29c
Dustin Dobervich <ddobervich@gmail.com>
2012-01-25 16:43:11 -0600

    quickly added a few tests

---
+++ Tests/DummyEntity.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Vich\UploaderBundle\Tests;
+
+use Vich\UploaderBundle\Annotation as Vich;
+
+/**
+ * @Vich\Uploadable
+ */
+class DummyEntity
+{
+    /**
+     * @Vich\UploadableField(mapping="dummy_file", fileNameProperty="fileName")
+     */
+    protected $file;
+
+    protected $fileName;
+
+    public function getFile()
+    {
+        $this->file;
+    }
+
+    public function setFile($file)
+    {
+        $this->file = $file;
+    }
+
+    public function getFileName()
+    {
+        return $this->fileName;
+    }
+
+    public function setFileName($fileName)
+    {
+        $this->fileName = $fileName;
+    }
+}
