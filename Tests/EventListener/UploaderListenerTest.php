d7f682852ecc700274a8cdf89a0fc915695e4455
Dustin Dobervich <ddobervich@gmail.com>
2012-01-28 19:17:51 -0600

    added gitignore

---
+++ Tests/EventListener/UploaderListenerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Vich\UploaderBundle\Tests\EventListener;
+
+/**
+ * UploaderListenerTest.
+ *
+ * @author Dustin Dobervich <ddobervich@gmail.com>
+ */
+class UploaderListenerTest extends \PHPUnit_Framework_TestCase
+{
+
+}
