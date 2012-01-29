796a2ea3c0ee7ba2cdb91ea35d2287f8121b20a3
Dustin Dobervich <ddobervich@gmail.com>
2012-01-28 22:19:23 -0600

    added PropertyMapping test

---
+++ Tests/Mapping/ProperyMappingTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Vich\UploaderBundle\Tests\Mapping;
+
+use Vich\UploaderBundle\Mapping\PropertyMapping;
+
+/**
+ * PropertyMappingTest.
+ *
+ * @author Dustin Dobervich <ddobervich@gmail.com>
+ */
+class PropertyMappingTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test that the configured mappings are accessed
+     * correctly.
+     */
+    public function testConfiguredMappingAccess()
+    {
+        $prop = new PropertyMapping();
+        $prop->setMapping(array(
+            'delete_on_remove' => true,
+            'upload_dir' => '/tmp',
+            'inject_on_load' => true
+        ));
+
+        $this->assertEquals($prop->getUploadDir(), '/tmp');
+        $this->assertTrue($prop->getDeleteOnRemove());
+        $this->assertTrue($prop->getInjectOnLoad());
+    }
+}
