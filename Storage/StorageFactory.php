3e85046c1f9318e39c0b92d60507a8a5b62f6ae5
Francesco Tassi <tassi.francesco@gmail.com>
2012-04-15 10:22:33 +0200

    StorageFActory

---
+++ Storage/StorageFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Vich\UploaderBundle\Storage;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * StorageFactory
+ *
+ * @author ftassi
+ */
+class StorageFactory
+{
+
+    /**
+     *
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     *
+     * @param ContainerInterface $container 
+     */
+    function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function createStorage()
+    {
+        return $this->container->get($this->container->getParameter('vich_uploader.storage_service'));
+    }
+
+}
+
+?>
