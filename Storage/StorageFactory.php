701d82a39f3c5f263c03073e4964859028d4d358
Francesco Tassi <tassi.francesco@gmail.com>
2012-08-25 22:27:34 +0100

    Use StorageFactory to create vich_uploader.storage service
    Using the factory to instantiate the correct StorageInterface implementation
    allows to customize the StorageInterface based on storage config parameter

---
+++ Storage/StorageFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Vich\UploaderBundle\Storage;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * StorageFactory
+ *
+ * @author Francesco Tassi <tassi.francesco@gmail.com>
+ */
+class StorageFactory
+{
+
+    /**
+     *
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     *
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function createStorage()
+    {
+        return $this->container->get($this->container->getParameter('vich_uploader.storage_service'));
+    }
+
+}
