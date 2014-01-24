e0445e7a02c2d738cfd0cf9a4c86c96c375618f1
Kévin Gomez <geek63@gmail.com>
2014-01-24 16:26:18 +0000

    Added an XML driver

---
+++ Metadata/Driver/Xml.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Vich\UploaderBundle\Metadata\Driver;
+
+use Metadata\Driver\AbstractFileDriver;
+
+use Vich\UploaderBundle\Metadata\ClassMetadata;
+
+/**
+ * Xml driver
+ *
+ * @author Kévin Gomez <contact@kevingomez.fr>
+ */
+class Xml extends AbstractFileDriver
+{
+    protected function loadMetadataFromFile(\ReflectionClass $class, $file)
+    {
+        $previous = libxml_use_internal_errors(true);
+        $elem = simplexml_load_file($file);
+        libxml_use_internal_errors($previous);
+
+        if (false === $elem) {
+            throw new \RuntimeException(libxml_get_last_error());
+        }
+
+        $metadata = new ClassMetadata($class->name);
+
+        foreach ($elem->children() as $field) {
+            $fieldMetadata = array(
+                'mapping'           => (string) $field->attributes()->mapping,
+                'propertyName'      => (string) $field->attributes()->name,
+                'fileNameProperty'  => (string) $field->attributes()->filename_property,
+            );
+
+            $metadata->fields[(string) $field->attributes()->name] = $fieldMetadata;
+        }
+
+        return $metadata;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getExtension()
+    {
+        return 'xml';
+    }
+}
