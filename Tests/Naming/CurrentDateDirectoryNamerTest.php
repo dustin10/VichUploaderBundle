7240abf3066fa2462a9a46a796769467bafd4350
David Romaní <david@flux.cat>
2014-02-26 12:26:22 +0100

    Current date directory namer

--- Tests/Naming/CurrentDateDirectoryNamerTest.php
+++ Tests/Naming/CurrentDateDirectoryNamerTest.php
@@ -12,35 +12,14 @@ use Vich\UploaderBundle\Tests\DummyEntity;
  */
 class CurrentDateDirectoryNamerTest extends \PHPUnit_Framework_TestCase
 {
-    public function fileDataProvider()
-    {
-        return array(
-            //    original_name,    guessed_extension,  pattern
-            array('lala.jpeg',      null,               '/[a-z0-9]{13}.jpeg/'),
-            array('lala.mp3',       'mpga',             '/[a-z0-9]{13}.mp3/'),
-            array('lala',           'mpga',             '/[a-z0-9]{13}.mpga/'),
-            array('lala',           null,               '/[a-z0-9]{13}/'),
-        );
-    }
-
     /**
-     * @dataProvider fileDataProvider
+     * Test Current Date Directory Namer
      */
-    public function testDirectoryNameReturnsACurrentDate($originalName, $guessedExtension, $pattern)
+    public function testDirectoryNameReturnsACurrentDate()
     {
         $file = $this->getMockBuilder('Symfony\Component\HttpFoundation\File\UploadedFile')
             ->disableOriginalConstructor()
             ->getMock();
-
-        $file
-            ->expects($this->any())
-            ->method('getClientOriginalName')
-            ->will($this->returnValue($originalName));
-
-        $file
-            ->expects($this->any())
-            ->method('guessExtension')
-            ->will($this->returnValue($guessedExtension));
 
         $entity = new DummyEntity;
         $entity->setFile($file);
