066cfc637d940bc63057e64aefba4f9d12e28063
Andrew D Battye <andrewbattye@leadershipfactor.com>
2014-04-08 10:03:59 +0100

    set signature of directoryName method in DirectoryNamerInterface to match documentation

--- Naming/DirectoryNamerInterface.php
+++ Naming/DirectoryNamerInterface.php
@@ -14,12 +14,11 @@ interface DirectoryNamerInterface
     /**
      * Creates a directory name for the file being uploaded.
      *
-     * @param object          $object  The object the upload is attached to.
-     * @param Propertymapping $mapping The mapping to use to manipulate the given object.
-     *
-     * @note The $object parameter can be null.
+     * @param object $obj       The object the upload is attached to.
+     * @param string $field     The name of the uploadable field to generate a name for.
+     * @param string $uploadDir The upload directory set in config
      *
      * @return string The directory name.
      */
-    public function directoryName($object, PropertyMapping $mapping);
+    public function directoryName($obj, $field, $uploadDir);
 }
