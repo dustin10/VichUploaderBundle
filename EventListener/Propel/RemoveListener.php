957026aa0dab47e70757de7ca0145ef298870e97
Kévin Gomez <geek63@gmail.com>
2014-06-20 21:02:08 +0100

    Split propel listeners

---
+++ EventListener/Propel/RemoveListener.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Vich\UploaderBundle\EventListener\Propel;
+
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+/**
+ * RemoveListener
+ *
+ * Listen to the remove event to delete files accordingly.
+ *
+ * @author Kévin Gomez <contact@kevingomez.fr>
+ */
+class RemoveListener extends BaseListener
+{
+    /**
+     * The events the listener is subscribed to.
+     *
+     * @return array The array of events.
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            'propel.post_delete' => 'onDelete',
+        );
+    }
+
+    /**
+     * Removes the file when the object is deleted.
+     *
+     * @param GenericEvent $event The event.
+     */
+    public function onDelete(GenericEvent $event)
+    {
+        $object = $this->adapter->getObjectFromArgs($event);
+        $this->handler->delete($object, $this->mapping);
+    }
+}
