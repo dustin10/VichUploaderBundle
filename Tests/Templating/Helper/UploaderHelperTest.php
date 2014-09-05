60e918bda892d78fa5eca6568335fc954b62544f
Francesco Tassi <tassi.francesco@gmail.com>
2014-09-05 12:12:38 +0200

    Try to determine asset path from mapping name or from field name

--- Tests/Templating/Helper/UploaderHelperTest.php
+++ Tests/Templating/Helper/UploaderHelperTest.php
@@ -4,26 +4,78 @@
 namespace Vich\UploaderBundle\Tests\Templating\Helper;
 
 
+use PHPUnit_Framework_MockObject_MockObject;
+use Vich\UploaderBundle\Exception\MissingMappingException;
 use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
 
 class UploaderHelperTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $storage;
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $mappingFactory;
+
+    protected function setUp()
+    {
+        $this->storage = $this->getMockBuilder('Vich\UploaderBundle\Storage\StorageInterface')
+            ->getMock();
+
+        $this->mappingFactory = $this->getMockBuilder('Vich\UploaderBundle\Mapping\PropertyMappingFactory')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->helper = new UploaderHelper($this->storage, $this->mappingFactory);
+    }
+
+
     public function testAssetShouldResolvePathUsingMappingName()
     {
         $obj = new \stdClass();
-        $mappingName = 'foo_mapping_name';
+        $fileIdentifier = 'foo_mapping_name';
         $className = null;
 
-        $storage = $this->getMockBuilder('Vich\UploaderBundle\Storage\StorageInterface')
-            ->getMock();
-
-        $storage->expects($this->atLeastOnce())
+        $this->storage->expects($this->atLeastOnce())
             ->method('resolveUri')
-            ->with($obj, $mappingName, $className)
+            ->with($obj, $fileIdentifier, $className)
             ->willReturn('foo/asset/path');
 
-        $helper = new UploaderHelper($storage);
+        $this->assertEquals('foo/asset/path', $this->helper->asset($obj, $fileIdentifier, $className));
+    }
 
-        $this->assertEquals('foo/asset/path', $helper->asset($obj, $mappingName, $className));
+    public function testAssetShouldFallbackToFieldIfMappingNameDoesNotExists()
+    {
+        $obj = new \stdClass();
+        $fileIdentifier = 'objectFieldName';
+        $className = null;
+
+        $propertyMapping = $this->getMockBuilder('Vich\UploaderBundle\Mapping\PropertyMapping')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $propertyMapping->expects($this->any())
+            ->method('getMappingName')
+            ->willReturn('foo_mapping_name');
+
+        $this->storage->expects($this->at(0))
+            ->method('resolveUri')
+            ->with($obj, $fileIdentifier, $className)
+            ->willThrowException(new MissingMappingException('Mapping foo_mapping_name does not exist'));
+
+        $this->mappingFactory->expects($this->any())
+            ->method('fromField')
+            ->with($obj, $fileIdentifier, $className)
+            ->willReturn($propertyMapping);
+
+        $this->storage->expects($this->at(1))
+            ->method('resolveUri')
+            ->with($obj, 'foo_mapping_name', $className)
+            ->willReturn('foo/asset/path');
+
+        $this->assertEquals('foo/asset/path', $this->helper->asset($obj, $fileIdentifier, $className));
     }
 }
