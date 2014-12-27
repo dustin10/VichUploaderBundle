74159f211bd323d761a77e9166b5b906d6e88383
Kévin Gomez <geek63@gmail.com>
2014-12-27 13:20:39 +0000

    Fix tests for symfony 2.7.x-dev

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,17 +1,11 @@
 <?php
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
-use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+error_reporting(E_ALL & ~E_USER_DEPRECATED);
 
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
-
-// dirty hack to autoload vich annotations
-new Vich\Uploadable();
-new Vich\UploadableField(array(
-    'mapping'          => 'foo',
-    'fileNameProperty' => 'bar',
-));
 
 return $loader;
