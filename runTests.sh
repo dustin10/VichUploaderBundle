296c255745159624b68f2f641925eb4e5f9ec4c5
Restless-ET <restless86@gmail.com>
2015-06-30 16:34:12 +0100

    Removed installation of PHP related packages from the runtests.sh script.

--- runTests.sh
+++ runTests.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
 
-#export SYMFONY_VERSION="~2.7.0"
 SUPPORTED_SYMFONY_VERSIONS=('~2.3.0' '~2.5.0' '~2.6.0' '~2.7.0' '2.8.x-dev')
 export DOCTRINE_VERSION=">=2.2.3,<2.5-dev"
 
-sudo apt-get -qq update
-sudo apt-get install -y php5 php5-mongo php5-sqlite
-php --version
+restore_composer (){
+  # Restore the composer.json file
+  rm composer.json && mv composer.json.bck composer.json
+}
 
 # Install/upgrade composer.phar
 curl -sS https://getcomposer.org/installer | php
@@ -29,11 +29,10 @@ for sf_version in ${SUPPORTED_SYMFONY_VERSIONS[@]}; do
   php composer.phar require --no-update "symfony/symfony":"${sf_version}"
   php composer.phar require --no-update "doctrine/orm":"${DOCTRINE_VERSION}"
 
-  php composer.phar update symfony/symfony
-  php composer.phar install --prefer-source
+  php composer.phar update symfony/symfony || { restore_composer && exit 1; }
+  php composer.phar install --prefer-source || { restore_composer && exit 1; }
 
   vendor/bin/phpunit $@
 done
 
-# Restore the composer.json file
-rm composer.json && mv composer.json.bck composer.json
+restore_composer
