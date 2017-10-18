53ecd587bb399b13f127fe09afcad802a466c31a
Raynald <raynald@easi-services.fr>
2017-10-18 19:42:56 +0200

    Add transliteration capabilities to property directory namer

--- Naming/PropertyDirectoryNamer.php
+++ Naming/PropertyDirectoryNamer.php
@@ -7,6 +7,7 @@ use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 use Vich\UploaderBundle\Exception\NameGenerationException;
 use Vich\UploaderBundle\Mapping\PropertyMapping;
+use Vich\UploaderBundle\Util\Transliterator;
 
 /**
  * Directory namer which can create subfolder depends on property.
@@ -19,12 +20,17 @@ class PropertyDirectoryNamer implements DirectoryNamerInterface, ConfigurableInt
      * @var string
      */
     private $propertyPath;
-
+    
+    /**
+     * @var bool
+     */
+    private $transliterate = false;
+    
     /**
      * @var PropertyAccessorInterface
      */
     protected $propertyAccessor;
-
+    
     /**
      * @param PropertyAccessorInterface $propertyAccessor Property accessor interface
      */
@@ -32,20 +38,22 @@ class PropertyDirectoryNamer implements DirectoryNamerInterface, ConfigurableInt
     {
         $this->propertyAccessor = $propertyAccessor ?: PropertyAccess::createPropertyAccessor();
     }
-
+    
     /**
      * @param array $options Options for this namer. The following options are accepted:
      *                       - property: path to the property used to name the file. Can be either an attribute or a method.
+     *                       - transliterate: whether the filename should be transliterated or not
      */
     public function configure(array $options)
     {
         if (empty($options['property'])) {
             throw new \InvalidArgumentException('Option "property" is missing or empty.');
         }
-
+        
         $this->propertyPath = $options['property'];
+        $this->transliterate = isset($options['transliterate']) ? (bool) $options['transliterate'] : $this->transliterate;
     }
-
+    
     /**
      * {@inheritdoc}
      */
@@ -54,17 +62,21 @@ class PropertyDirectoryNamer implements DirectoryNamerInterface, ConfigurableInt
         if (empty($this->propertyPath)) {
             throw new \LogicException('The property to use can not be determined. Did you call the configure() method?');
         }
-
+        
         try {
             $name = $this->propertyAccessor->getValue($object, $this->propertyPath);
         } catch (NoSuchPropertyException $e) {
             throw new NameGenerationException(sprintf('Directory name could not be generated: property %s does not exist.', $this->propertyPath), $e->getCode(), $e);
         }
-
+        
         if (empty($name)) {
             throw new NameGenerationException(sprintf('Directory name could not be generated: property %s is empty.', $this->propertyPath));
         }
-
+        
+        if ($this->transliterate) {
+            $name = Transliterator::transliterate($name);
+        }
+        
         return $name;
     }
 }
