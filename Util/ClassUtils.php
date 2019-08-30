ef4366a9c0c83cbfb8e914cf5902d6fd0b435bbe
atari <ukhardc0r3@gmail.com>
2019-08-30 08:15:19 +0400

    Compatibility with doctrine/mongodb-odm v.2.0; Drop DoctrineClassUtils;

--- Util/ClassUtils.php
+++ Util/ClassUtils.php
@@ -22,10 +22,25 @@ class ClassUtils
      */
     public static function getClass($object): string
     {
-        if (\class_exists(DoctrineClassUtils::class)) {
-            return DoctrineClassUtils::getClass($object);
+        $className = \get_class($object);
+
+        // __CG__: Doctrine Common Marker for Proxy (ODM < 2.0 and ORM < 3.0)
+        // __PM__: Ocramius Proxy Manager (ODM >= 2.0)
+        if ((false === $positionCg = strrpos($className, '\\__CG__\\')) &&
+            (false === $positionPm = strrpos($className, '\\__PM__\\'))) {
+            return $className;
         }
 
-        return \get_class($object);
+        if (false !== $positionCg) {
+            return substr($className, $positionCg + 8);
+        }
+
+        $className = ltrim($className, '\\');
+
+        return substr(
+            $className,
+            8 + $positionPm,
+            strrpos($className, '\\') - ($positionPm + 8)
+        );
     }
 }
