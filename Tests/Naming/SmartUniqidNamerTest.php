2eeb62f7572a0a41bab0c5a366a5d0afa4e00a7d
Austin Ewens <email@aewens.com>
2019-10-31 11:03:30 -0500

    Fix regex used in new tests

--- Tests/Naming/SmartUniqidNamerTest.php
+++ Tests/Naming/SmartUniqidNamerTest.php
@@ -16,10 +16,10 @@ final class SmartUniqidNamerTest extends TestCase
             'accented' => ['làlà.mp3', '/lala-[[:xdigit:]]{22}\.mp3/'],
             'spaced' => ['a Foo Bar.txt', '/a-foo-bar-[[:xdigit:]]{22}\.txt/'],
             'special char' => ['yezz!.png', '/yezz-[[:xdigit:]]{22}\.png/'],
-            'long basename' => [\str_repeat('a', 230).'.txt', '/a{229}[[:xdigit:]]{22}\.txt/'],
-            'long extension' => ['a.'.\str_repeat('a', 232), '/a[[:xdigit:]]{22}\.a{231}/'],
+            'long basename' => [\str_repeat('a', 230).'.txt', '/a{228}-[[:xdigit:]]{22}\.txt/'],
+            'long extension' => ['a.'.\str_repeat('a', 232), '/a-[[:xdigit:]]{22}\.a{230}/'],
             'long basename and extension' => [\str_repeat('a', 237).'.txt'.\str_repeat('a', 256),
-                                              '/[[:xdigit:]]{22}\.txt/', ],
+                                              '/a{228}-[[:xdigit:]]{22}\.txt/', ],
         ];
     }
 
