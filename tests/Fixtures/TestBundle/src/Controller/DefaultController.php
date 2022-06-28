44fdfbcfeda7a50f3edc465d9e0966395c069662
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2022-06-28 19:02:22 +0200

    Allow the use of propertyPath (#1293)
    Allow the use of propertyPath

--- tests/Fixtures/TestBundle/src/Controller/DefaultController.php
+++ tests/Fixtures/TestBundle/src/Controller/DefaultController.php
@@ -33,11 +33,32 @@ class DefaultController extends AbstractController
         ]);
     }
 
+    public function uploadWithPropertyPathAction(string $formType): Response
+    {
+        $form = $this->getFormWithPropertyPath($this->getImage());
+
+        return $this->render('default/upload_with_property_path.html.twig', [
+            'formType' => $formType,
+            'form' => $form->createView(),
+        ]);
+    }
+
     public function editAction(string $formType, ?int $imageId): Response
     {
         $form = $this->getForm($this->getImage($imageId));
 
         return $this->render('default/edit.html.twig', [
+            'imageId' => $imageId,
+            'formType' => $formType,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    public function editWithPropertyPathAction(string $formType, ?int $imageId): Response
+    {
+        $form = $this->getFormWithPropertyPath($this->getImage($imageId));
+
+        return $this->render('default/edit_with_property_path.html.twig', [
             'imageId' => $imageId,
             'formType' => $formType,
             'form' => $form->createView(),
@@ -67,11 +88,44 @@ class DefaultController extends AbstractController
         ]);
     }
 
+    public function submitWithPropertyPathAction(Request $request, string $formType, ?int $imageId = null): Response
+    {
+        $image = $this->getImage($imageId);
+        $form = $this->getFormWithPropertyPath($image)->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->doctrine->getManager();
+
+            $em->persist($image);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('view_with_property_path', [
+                'formType' => $formType,
+                'imageId' => $image->getId(),
+            ]));
+        }
+
+        return $this->render('default/upload_with_property_path.html.twig', [
+            'formType' => $formType,
+            'form' => $form->createView(),
+        ]);
+    }
+
     private function getForm(Image $image): FormInterface
     {
         return $this->createFormBuilder($image)
             ->add('title', Type\TextType::class)
             ->add('imageFile', VichType\VichImageType::class)
+            ->add('save', Type\SubmitType::class)
+            ->getForm()
+        ;
+    }
+
+    private function getFormWithPropertyPath(Image $image): FormInterface
+    {
+        return $this->createFormBuilder($image)
+            ->add('title', Type\TextType::class)
+            ->add('image_file', VichType\VichImageType::class, ['property_path' => 'imageFile'])
             ->add('save', Type\SubmitType::class)
             ->getForm()
         ;
