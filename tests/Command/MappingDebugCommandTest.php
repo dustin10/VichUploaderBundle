f4079e42fcd617857cf017941a44a8b3963dc8db
Manuel Dalla Lana <manuel@pontedilana.it>
2022-07-18 16:31:22 +0200

    Add command completion for vich:mapping:debug
    Partially fixes #1232

--- tests/Command/MappingDebugCommandTest.php
+++ tests/Command/MappingDebugCommandTest.php
@@ -2,6 +2,7 @@
 
 namespace Vich\UploaderBundle\Tests\Command;
 
+use Symfony\Component\Console\Tester\CommandCompletionTester;
 use Vich\UploaderBundle\Command\MappingDebugCommand;
 use Vich\UploaderBundle\Exception\MappingNotFoundException;
 
@@ -21,5 +22,36 @@ final class MappingDebugCommandTest extends AbstractCommandTestCase
         $command = new MappingDebugCommand($mappings);
         $output = $this->executeCommand('vich:mapping:debug', $command, ['mapping' => 'image_mapping']);
         self::assertStringContainsString('Debug information for mapping image_mapping', $output);
+    }
+
+    /**
+     * @dataProvider provideCompletionSuggestions
+     */
+    public function testComplete(array $input, array $expectedSuggestions): void
+    {
+        if (!class_exists(CommandCompletionTester::class)) {
+            $this->markTestSkipped('Test command completion requires symfony/console 5.4+.');
+        }
+
+        $mappings = [
+            'product_mapping' => [],
+            'taxonomy_mapping' => [],
+            'image_mapping' => [],
+        ];
+        $tester = new CommandCompletionTester(new MappingDebugCommand($mappings));
+
+        $this->assertEqualsCanonicalizing($expectedSuggestions, $tester->complete($input));
+    }
+
+    public function provideCompletionSuggestions(): \Generator
+    {
+        yield 'mapping' => [
+            [''],
+            [
+                'image_mapping',
+                'product_mapping',
+                'taxonomy_mapping',
+            ],
+        ];
     }
 }
