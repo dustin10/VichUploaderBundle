b35bad607e9923b655e2a30bd5a7db265f48db00
Massimiliano Arione <garakkio@gmail.com>
2023-02-13 17:05:30 +0100

    make Twig extension lazy

--- src/Twig/Extension/UploaderExtension.php
+++ src/Twig/Extension/UploaderExtension.php
@@ -4,36 +4,16 @@ namespace Vich\UploaderBundle\Twig\Extension;
 
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
-use Vich\UploaderBundle\Templating\Helper\UploaderHelperInterface;
 
 /**
- * UploaderExtension.
- *
  * @author Dustin Dobervich <ddobervich@gmail.com>
  */
 final class UploaderExtension extends AbstractExtension
 {
-    public function __construct(private readonly UploaderHelperInterface $helper)
-    {
-    }
-
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('vich_uploader_asset', $this->asset(...)),
+            new TwigFunction('vich_uploader_asset', [UploaderExtensionRuntime::class, 'asset']),
         ];
-    }
-
-    /**
-     * Gets the public path for the file associated with the uploadable object.
-     *
-     * @param object      $object    The object
-     * @param string|null $fieldName The field name
-     *
-     * @return string|null The public path or null if file not stored
-     */
-    public function asset(object $object, ?string $fieldName = null): ?string
-    {
-        return $this->helper->asset($object, $fieldName);
     }
 }
