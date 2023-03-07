08c30dc76ecb6323b60c665afbf9a7f37cea9d4d
Ion Bazan <ion.bazan@gmail.com>
2023-03-07 16:41:35 +0800

    Simplify tests

--- tests/Adapter/ODM/MongoDB/MongoDBAdapterTest.php
+++ tests/Adapter/ODM/MongoDB/MongoDBAdapterTest.php
@@ -2,11 +2,8 @@
 
 namespace Vich\UploaderBundle\Tests\Adapter\ODM\MongoDB;
 
-use Doctrine\Common\EventManager;
-use Doctrine\ODM\MongoDB\Configuration;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ODM\MongoDB\Event\PreUpdateEventArgs;
-use Doctrine\ODM\MongoDB\Hydrator\HydratorFactory;
 use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
 use Doctrine\ODM\MongoDB\UnitOfWork;
 use PHPUnit\Framework\TestCase;
@@ -30,32 +27,15 @@ final class MongoDBAdapterTest extends TestCase
             ],
         ];
         $dm = $this->createStub(DocumentManager::class);
-        $dm->method('getClassMetadata')->willReturn(new ClassMetadata(DummyEntity::class));
+        $metadata = new ClassMetadata(DummyEntity::class);
+        $dm->method('getClassMetadata')->willReturn($metadata);
         $event = new PreUpdateEventArgs($entity, $dm, $changeSet);
 
         $adapter = new MongoDBAdapter();
-        $uow = $this->getUnitOfWork($dm);
+        $uow = $this->createMock(UnitOfWork::class);
         $dm->method('getUnitOfWork')->willReturn($uow);
         $uow->persist($entity);
-
-        $uow->setDocumentChangeSet($entity, $changeSet);
-
-        self::assertSame($changeSet, $uow->getDocumentChangeSet($entity));
+        $uow->expects(self::once())->method('recomputeSingleDocumentChangeSet')->with($metadata, $entity);
         $adapter->recomputeChangeSet($event);
-        self::assertSame([], $uow->getDocumentChangeSet($entity));
-    }
-
-    protected function getUnitOfWork(DocumentManager $documentManager): UnitOfWork
-    {
-        $eventManager = $this->createStub(EventManager::class);
-        $hydratorFactory = new HydratorFactory(
-            $documentManager,
-            $eventManager,
-            '/tmp',
-            'Hydrator',
-            Configuration::AUTOGENERATE_NEVER
-        );
-
-        return new UnitOfWork($documentManager, $eventManager, $hydratorFactory);
     }
 }
