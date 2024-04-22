e89dfcfa1fc10abee4f0c924fbd2184f37eaf3f5
Douglas Hammond <wizhippo@gmail.com>
2024-04-22 13:44:03 -0400

    Fix issue 1445
    https://github.com/dustin10/VichUploaderBundle/issues/1445

--- src/Storage/FlysystemStorage.php
+++ src/Storage/FlysystemStorage.php
@@ -6,6 +6,7 @@ use League\Flysystem\FilesystemException;
 use League\Flysystem\FilesystemOperator;
 use League\Flysystem\MountManager;
 use Psr\Container\ContainerInterface;
+use Symfony\Component\ErrorHandler\Error\UndefinedMethodError;
 use Symfony\Component\HttpFoundation\File\Exception\CannotWriteFileException;
 use Symfony\Component\HttpFoundation\File\File;
 use Vich\UploaderBundle\Mapping\PropertyMapping;
@@ -120,7 +121,7 @@ final class FlysystemStorage extends AbstractStorage
 
         try {
             return $fs->publicUrl($path);
-        } catch (FilesystemException) {
+        } catch (FilesystemException | UndefinedMethodError) {
             return $mapping->getUriPrefix().'/'.$path;
         }
     }
