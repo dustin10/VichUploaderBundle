e89c8bb6c4d6eaae668c9f9fdb7a9eac6e495d62
Regis Martini <regis.martini@gmail.com>
2024-10-30 22:23:31 +0100

    Updated constructor call that requires 2 parameters

--- tests/Storage/Flysystem/AbstractFlysystemStorageTestCase.php
+++ tests/Storage/Flysystem/AbstractFlysystemStorageTestCase.php
@@ -2,6 +2,7 @@
 
 namespace Vich\UploaderBundle\Tests\Storage\Flysystem;
 
+use Error;
 use League\Flysystem\Filesystem;
 use League\Flysystem\FilesystemAdapter;
 use League\Flysystem\FilesystemOperator;
@@ -219,7 +220,7 @@ abstract class AbstractFlysystemStorageTestCase extends StorageTestCase
             ->expects(self::once())
             ->method('publicUrl')
             ->with('file.txt')
-            ->will($this->throwException(new UndefinedMethodError('Undefined method')));
+            ->will($this->throwException(new UndefinedMethodError('Undefined method'), new Error('An error occurred')));
 
         $this->mapping
             ->expects(self::once())
