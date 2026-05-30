0c6759abcff0eded37c6490a258f69251514a000
Massimiliano Arione <garakkio@gmail.com>
2026-05-30 11:17:05 +0200

    other fixes

--- tests/BypassFinalExtension.php
+++ tests/BypassFinalExtension.php
@@ -5,13 +5,14 @@ namespace Vich\UploaderBundle\Tests;
 use DG\BypassFinals;
 use PHPUnit\Runner\Extension\Extension;
 use PHPUnit\Runner\Extension\Facade;
-use PHPUnit\TextUI\Configuration\Configuration;
 use PHPUnit\Runner\Extension\ParameterCollection;
+use PHPUnit\TextUI\Configuration\Configuration;
 
 final class BypassFinalExtension implements Extension
 {
     public function bootstrap(Configuration $configuration, Facade $facade, ParameterCollection $parameters): void
     {
+        BypassFinals::denyPaths(['*/vendor/phpunit/*']);
         BypassFinals::enable();
     }
 }
